ce27726b5af63838553619825cc5ee4854daba19
jaceseol <seolju90@gmail.com>
2023-03-27 14:11:53 +0900

    추가 : 폰켓몬문제 좋은 솔루션 추가

--- 2023_03_27_phoneKetMon.js
+++ 2023_03_27_phoneKetMon.js
@@ -8,3 +8,11 @@ function solution(nums) {
 
   return answer;
 }
+
+// 좋은 솔루션
+function solution(nums) {
+  const max = nums.length / 2;
+  const arr = [...new Set(nums)];
+
+  return arr.length > max ? max : arr.length;
+}
